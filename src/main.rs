770176cce851f8e964b2bf54f47c58808972d16e
mdgeno <manuelgenomer@gmail.com>
2025-09-19 02:29:34 +0800

    created the using derived traits branch, to test out the project using a few of the derived debug syntax

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]    //step 1 for debugging structs
 struct Rectangle{
 	width: u32,
 	height: u32,
@@ -5,15 +6,24 @@ struct Rectangle{
 
 fn main(){
 
+	let width_val = 30 *2;
+
+	let name_val = String::from("recta");
+
 	let rect1 = Rectangle{
-		width: 30,
+		width: dbg!(width_val), //step 2 if dbg!
 		height: 50,
 	};
 
-	println!("The area of the rectangle is {}", area(&rect1));
+	println!("The area of the rectangle is {rect1:?}");  //step 2
+
+	println!("The area of the rectangle is {rect1:#?}");   //or step 2
+
+	dbg!(&rect1);  //another dbg! step
 
 }
-
+/*
 fn area(rectangle: &Rectangle) -> u32{
 	rectangle.width*rectangle.height
 }
+*/ 
