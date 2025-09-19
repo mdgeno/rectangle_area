6616526ef259b04467f220922dbdea47039db16d
mdgeno <manuelgenomer@gmail.com>
2025-09-19 23:59:29 +0800

    added some further refactor with the struct, implemented an implementation block that contains the method area, which was originally a function outside the main function.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,51 @@
+//create rectangle struct.
+//apply derived debug features to test how the vairables work.
+#[derive(Debug)]
 struct Rectangle{
 	width: u32,
 	height: u32,
 }
 
+//create an implementation block for the area method
+impl Rectangle{
+
+	//when creating a method in the implementation block, 
+	//type in the long form of the required initial self 
+	//parameter.
+	fn area(self: &Self) -> u32{
+		self.width*self.height
+	}	
+		
+}
+
+//create a main function
 fn main(){
 
+	//initialise an instance of the Rectangle struct.
 	let rect1 = Rectangle{
-		width: 30,
+		width: dbg!(30*2),
 		height: 50,
-	};
+	};	
 
-	println!("The area of the rectangle is {}", area(&rect1));
+	//print line macro the text ' ' and utilising the 
+	//implemented method of the Rectangle struct.
+	println!("The area of the rectangle is {}", rect1.area());
+	
+	println!(" ");
 
+	//use normal :? output and :#? pretty print
+	println!("the value of rect1 is {rect1:?}");
+	println!("the value of rect1 with pretty print {rect1:#?}");
+
+	//use the dbg! macro
+	dbg!(rect1.width);
 }
 
-fn area(rectangle: &Rectangle) -> u32{
-	rectangle.width*rectangle.height
-}
+
+
+
+
+
+
+
+
