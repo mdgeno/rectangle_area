fb61b711527b6ec2d9241c69b557566a15825311
mdgeno <manuelgenomer@gmail.com>
2025-09-21 05:02:51 +0800

    created a branch for adding a feature that is able to create a square concept out of the Rectangle struct

--- src/main.rs
+++ src/main.rs
@@ -16,17 +16,21 @@ impl Rectangle{
 	}
 }
 
+impl Rectangle{
+	fn square(size: u32) -> Self{
+		Self{
+			width: size,
+			height: size,
+		}
+	}
+}
+
 fn main(){
-
-	let mut user_input = String::new();
-	io::stdin().read_line(&mut user_input).expect("enter valid input");
-
-	println!("user input was: {}", &user_input);
 
 	let rect1 = Rectangle{
 		width: 30,
 		height: 50,
-	};	
+	};
 	let rect2 = Rectangle{
 		width: 10,
 		height: dbg!(40*1),
@@ -36,16 +40,60 @@ fn main(){
 		height: 45,
 	};
 
-	println!("Can rect1 hold rect2? {}", rect1.hold(&rect2));
-	println!("Can rect1 hold rect3? {}", rect1.hold(&rect3));
+	println!("Can rect1 hold rect2: {}", rect1.hold(&rect2));
+	println!("Can rect1 hold rect3: {}", rect1.hold(&rect3));
 
-	println!(" ");
+	println!("");
 
-	println!("derived standard format of rectangle 1 variable {rect1:?}");
-	println!("derived standard format of rectangle 2 variable {rect2:?}");
-	println!("derived pretty print format of rectangle 3 variable {rect3:#?}");
+	println!("Testing rect1 variable with derive trait debug: {rect1:?}");
+	println!("Testing rect2 variable with derive trait debug: {rect2:?}");
 
-	println!(" ");
+	println!("");
 
 	dbg!(rect2.height);
+
+	println!("");
+
+	loop{
+		println!("please enter size of square");
+
+		let mut user_in = String::new();
+		io::stdin().read_line(&mut user_in).expect("enter valid input");
+		
+		let user_in: u32 = match user_in.trim().parse(){
+					Ok(num) => num,
+					Err(_) => continue,
+				   }; 
+	
+		let new_square = Rectangle::square(user_in);
+
+		println!("Checking the value of the new_square variable: {new_square:?}");
+	
+		break;
+	}   
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
